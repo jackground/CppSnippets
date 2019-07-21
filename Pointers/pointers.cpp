17e9b6a3150af5959bcdaba041803a1de0fa9509
jackground <jackground@icloud.com>
2019-07-20 20:39:38 -0700

    Pointers

---
+++ Pointers/pointers.cpp
@@ -0,0 +1,20 @@
+/**
+ * Pointers
+ *
+ * Copyright (c) 2019, Jack Hawley.
+ */
+
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl;
+
+int main() {
+
+  int var = 8;
+  int* ptr = &var;
+  *ptr = 10;
+  LOG(var);
+  std::cin.get();
+
+  return 0;
+}
